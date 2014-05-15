0f0b7e64a18638704ac807e7b747beac70fa8cb1
L3ftsid3 <daniel.belleli@gmail.com>
2014-05-15 15:30:28 -0400

    Comments and Implementation Added

--- Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex1mapper.py
+++ Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex1mapper.py
@@ -6,3 +6,16 @@
 #    Find the total sales value across all the stores, and the total number of sales. Assume there is only one reducer.
 
 
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 4 (item) and 5 (cost) [KEY = item descr., VALUE = cost]
+# We need to write them out to std output, separated by a tab
+
+import sys
+
+for lin in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) == 6:
+        date, time, store, item, cost, payment = data
+        print"{0}\t{1}".format(item, cost)
