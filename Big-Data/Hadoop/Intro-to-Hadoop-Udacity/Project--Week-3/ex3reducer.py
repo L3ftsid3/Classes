5268ffd7ee7f1a9a0fe3118f7ec93fef63bf0fe7
L3ftsid3 <daniel.belleli@gmail.com>
2014-05-16 13:04:43 -0400

    Update ex3reducer.py

--- Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex3reducer.py
+++ Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex3reducer.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python
-
+ 
 import sys
-
-salesTotal = 0
-oldKey = None
-
+ 
+salesCount = 0
+totalSales = 0
+ 
 # Loop around the data
-# It will be in format key\tvalue
-# Where key is the item description, val is the sale amount
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
 #
-# All the sales for a particular item will be presented, 
-# then the key will change and we'll be dealing with the next item
-
+# All the sales will be saved in the two variables:
+# salesCount (number of sales)
+# totalSales (total Value of the sales)
+ 
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
     if len(data_mapped) != 2:
-        # Something has gone wrong. Skip the line
+        # Something has gone wrong. Skip this line.
         continue
-    
+ 
     thisKey, thisSale = data_mapped
-    
-    if oldKey and oldKey != thisKey:
-        print oldKey, "\t", salesTotal
-        oldKey = thisKey
-        salesTotal = 0
-      
-    oldKey = thisKey
-    salesTotal += float(thisSale)
-    
-if oldKey != None:
-    print oldKey, "\t", salesTotal
-    
+ 
+    salesCount += 1
+    totalSales += float(thisSale)
+ 
+print salesCount, "\t", totalSales
 
