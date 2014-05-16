f6e3a05d5284b77438161d0e994ec5c58074d7be
L3ftsid3 <daniel.belleli@gmail.com>
2014-05-16 12:38:11 -0400

    Update ex1mapper.py

--- Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex1mapper.py
+++ Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex1mapper.py
@@ -2,9 +2,6 @@
 
 #Excercise 1 - The three questions that you have to answer about this data set are:
 # Instead of breaking the sales down by store, instead give us a sales breakdown by product category across all of our stores.
-#    Find the monetary value for the highest individual sale for each separate store.
-#    Find the total sales value across all the stores, and the total number of sales. Assume there is only one reducer.
-
 
 # Format of each line is:
 # date\ttime\tstore name\titem description\tcost\tmethod of payment
