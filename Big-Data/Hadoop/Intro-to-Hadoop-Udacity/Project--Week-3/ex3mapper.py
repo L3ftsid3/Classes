3d4494a9f07559c46c6e0fc6a2aa5fab192c78d0
L3ftsid3 <daniel.belleli@gmail.com>
2014-05-16 11:39:13 -0400

    Inital Commit

--- Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex3mapper.py
+++ Big-Data/Hadoop/Intro-to-Hadoop-Udacity/Project--Week-3/ex3mapper.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 
 #Excercise 1 - The three questions that you have to answer about this data set are:
-# Instead of breaking the sales down by store, instead give us a sales breakdown by product category across all of our stores.
-#    Find the monetary value for the highest individual sale for each separate store.
 #    Find the total sales value across all the stores, and the total number of sales. Assume there is only one reducer.
 
 
 # Format of each line is:
 # date\ttime\tstore name\titem description\tcost\tmethod of payment
 #
-# We want elements 4 (item) and 5 (cost) [KEY = item descr., VALUE = cost]
+# We want elements 3 (store name) and 5 (cost) [KEY = store name, VALUE = cost]
 # We need to write them out to std output, separated by a tab
 
 import sys
